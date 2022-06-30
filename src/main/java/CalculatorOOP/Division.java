a63ecf2a309af2ff35168b4babc6738b72bf183d
KseniaSamarina <98531216+KseniaSamarina@users.noreply.github.com>
2022-06-30 22:27:42 +0300

    Исправила Задание №6_ Обработка исключений

--- src/main/java/CalculatorOOP/Division.java
+++ src/main/java/CalculatorOOP/Division.java
@@ -5,6 +5,9 @@ public class Division extends Action {
     public Division(double inputFirstNumber, double inputSecondNumber) {
         Action.inputFirstNumber = inputFirstNumber;
         Action.inputSecondNumber = inputSecondNumber;
+        if (inputSecondNumber == 0) {
+            throw new ArithmeticException("Double overflowed");
+        }
     }
 
     @Override
--- src/main/java/CalculatorOOP/Start.java
+++ src/main/java/CalculatorOOP/Start.java
@@ -4,21 +4,19 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Start {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         try {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter first number: ");
-        double inputFirstNumber = scanner.nextDouble();
-        System.out.println("Enter second number: ");
-        double inputSecondNumber = scanner.nextDouble();
-        System.out.println("Enter operation (+, -, *, /):");
-        char operation = scanner.next().charAt(0);
-        Calculator calculator = new Calculator(inputFirstNumber, operation, inputSecondNumber);
-            if (calculator.calculate().getResult() == Double.POSITIVE_INFINITY)
-                throw new ArithmeticException("Double overflowed");
-        System.out.println("Answer:");
-        System.out.printf("%.4f", calculator.calculate().getResult());
-        scanner.close();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter first number: ");
+            double inputFirstNumber = scanner.nextDouble();
+            System.out.println("Enter second number: ");
+            double inputSecondNumber = scanner.nextDouble();
+            System.out.println("Enter operation (+, -, *, /):");
+            char operation = scanner.next().charAt(0);
+            Calculator calculator = new Calculator(inputFirstNumber, operation, inputSecondNumber);
+            System.out.println("Answer:");
+            System.out.printf("%.4f", calculator.calculate().getResult());
+            scanner.close();
         } catch (InputMismatchException ex) {
             System.out.println("You entered an incorrect number");
         } catch (ArithmeticException ex1) {
